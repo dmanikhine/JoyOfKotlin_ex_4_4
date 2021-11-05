83520deeeb337f19ddd061b048a0a095b0e3454c
Denis Manikhin <dmanikhine@gmail.com>
2021-11-05 15:56:31 +0100

    add println in main

--- app/src/main/kotlin/ex_4_4/App.kt
+++ app/src/main/kotlin/ex_4_4/App.kt
@@ -10,6 +10,32 @@ class App {
         }
 }
 
+fun <T> List<T>.head(): T =
+        if (this.isEmpty())
+            throw IllegalArgumentException("head called on empty list")
+        else
+            this[0]
+
+fun <T> List<T>.tail(): List<T> =
+        if (this.isEmpty())
+            throw IllegalArgumentException("tail called on empty list")
+        else
+            this.drop(1)
+
+
+
+fun <T> makeString(list: List<T>, delim: String): String {
+    tailrec fun makeString_(list: List<T>, acc: String): String =
+		when {
+			list.isEmpty() -> acc
+			acc.isEmpty() -> makeString_(list.tail(), "${list.head()}")
+			else -> makeString_(list.tail(), "$acc$delim${list.head()}")
+		}
+	return makeString_(list, "")
+}
+
+
 fun main() {
     println(App().greeting)
+	println(makeString(listOf("a", "b", "c","d"), " delim "))
 }
